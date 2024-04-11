b930e74e8ae1f91ac8d419c2ab1eebf1e471e1c1
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-11 18:28:59 -0400

    added mobile styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import { Github, Linkedin, Twitter, Mail, } from "lucide-react";
+import { Github, Linkedin, Twitter, Mail } from "lucide-react";
 
 function Footer() {
   return (
     <footer className="bg-white text-black py-9 fixed bottom-0 w-full shadow-emerald-950 shadow-md ">
-      <div className="container mx-auto flex justify-between items-center ">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center"> {/* Added padding for different screen sizes */}
         {/* Div for the paragraph text */}
         <div className="">
           <p className="">Dont be shy, hit me up! 👉</p>
@@ -14,29 +14,33 @@ function Footer() {
             href="https://github.com/sbanati"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-black hover:text-blue-600">
+            className="text-black hover:text-blue-600"
+          >
             <Github size={30} />
           </a>
           <a
             href="https://www.linkedin.com/in/sahil-banati-a4172029b/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-black hover:text-blue-600">
-            <Linkedin size={30}  />
+            className="text-black hover:text-blue-600"
+          >
+            <Linkedin size={30} />
           </a>
           <a
             href="https://twitter.com/TherealSLVR"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-black hover:text-blue-600">
-            <Twitter size={30}  />
+            className="text-black hover:text-blue-600"
+          >
+            <Twitter size={30} />
           </a>
           <a
             href="mailto:smbanati@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-black hover:text-blue-600">
-            <Mail size={30}  />
+            className="text-black hover:text-blue-600"
+          >
+            <Mail size={30} />
           </a>
         </div>
       </div>
@@ -45,5 +49,3 @@ function Footer() {
 }
 
 export default Footer;
-
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="hero-section bg-cover bg-neutral-100 py-11 flex flex-col items-center justify-center min-h-screen mt-[-50px] sm:flex-row"> {/* Added flex-col and sm:flex-row classes */}
       <div className="max-w-4xl flex flex-col items-center justify-center mb-20 sm:flex-row"> {/* Added flex-col and sm:flex-row classes */}
         <div className="flex flex-col items-center justify-center mb-8 mr-0 sm:mr-12"> {/* Adjusted margin to provide more space */}
-          <h2 className="text-6xl mb-8 text-black text-center">Front-End Web Developer</h2> {/* Increased text size and centered text */}
+          <h2 className="text-6xl mb-8 text-black text-center">Full-Stack Web Developer</h2> {/* Increased text size and centered text */}
           <p className="text-xl text-black text-center">Hi! I am Sahil Banati, a new and passionate Front-end Developer from Toronto, Canada.🍁</p> {/* Increased text size and centered text */}
           <img src="https://skillicons.dev/icons?i=js,html,css,react,tailwind,nodejs,mysql,mongodb" alt="My Skills" className='pt-5 w-96 mx-auto sm:mx-0' /> {/* Centered the image and adjusted margin for mobile */}
         </div>
