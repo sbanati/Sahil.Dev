6c7c202aca63f9089152675956aa18dba6b341eb
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-11 17:03:02 -0400

    added About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,34 @@
-
-
+import screenshot2 from '../assets/screenshot2.png';
 
 const About = () => {
   return (
-    <div></div>
+    <section id="about" className="bg-neutral-200 py-20 pb-0 min-h-screen">
+      <div className="container mx-auto flex items-center justify-center">
+        <div className="max-w-4xl mx-auto flex items-center">
+          <div className="w-1/2 flex justify-center">
+            <div className="w-full md:w-3/4 flex justify-center">
+              <img
+                src={screenshot2}
+                alt="cartoon of boy coding on laptop"
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="w-1/2 px-8 py-6 flex items-center justify-center">
+            <div>
+              <h3 className="text-3xl font-bold text-blue-600 mb-4">About Me</h3>
+              <h4 className="text-2xl text-black mb-6">
+                A passionate, curious, and dedicated developer from Toronto, Canada.
+              </h4>
+              <p className="text-base text-gray-700 leading-relaxed">
+                As a new developer, I am focused on building strong fundamentals while learning new languages and developing skills for successful programming. I am a team player who loves to communicate and collaborate. What I lack in experience, I make up for with a strong will to learn while being driven by passion and curiosity.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default About
-
-
-
-
-
-
-
-
+export default About;
