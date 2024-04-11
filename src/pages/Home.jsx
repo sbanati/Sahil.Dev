5388f3dac6515150ee1e44c1ad18d9374537812c
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-11 15:59:56 -0400

    added tech stack to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@ import screenshot1 from '../assets/screenshot1.jpg';
 
 const Home = () => {
   return (
-    <div className="hero-section bg-cover bg-neutral-100 py-11 flex items-center justify-center min-h-screen">
+    <div className="hero-section bg-cover bg-neutral-100 py-11 flex items-center justify-center min-h-screen mt-[-50px] ">
       <div className="max-w-4xl flex items-center mb-20">
         <div className="flex flex-col justify-center mr-12"> {/* Adjusted margin to provide more space */}
           <h2 className="text-6xl mb-8 text-black">Front-End Web Developer</h2> {/* Increased text size */}
-          <p className="text-xl text-black ">Hi! I am Sahil Banati, a new and passionate Front-end Developer from Toronto, Canada.🍁</p> {/* Increased text size */}
+          <p className="text-xl text-black">Hi! I am Sahil Banati, a new and passionate Front-end Developer from Toronto, Canada.🍁</p> {/* Increased text size */}
+            <img src="https://skillicons.dev/icons?i=js,html,css,react,tailwind,nodejs,mysql,mongodb" alt="My Skills" className='pt-5 w-96' />
+          
         </div>
         <div className="ml-auto mt-12 hidden sm:block"> {/* Adjusted margin to provide more space */}
-          <img src={screenshot1} alt="Straw-Hat Pirate" className="h-50 rounded-full mb-12 " /> {/* Increased image height */}
+          <img src={screenshot1} alt="Straw-Hat Pirate" className="h-50 rounded-full mb-12  " /> {/* Increased image height */}
         </div>
       </div>
     </div>
