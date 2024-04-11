87146b484289e6703b6183920983476e18cc5cdc
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-11 18:19:21 -0400

    fixed hamburger navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,60 +5,59 @@ import "../styles/Navbar.css";
 
 function Navbar() {
   const currentPage = useLocation().pathname;
-  // This state will track the hamburger menu opening and closing 
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () =>{
-    // menu state toggle
+  const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
 
   return (
     <nav className="relative w-1/3 bg-white">
-      {/* Hamburger menu icon will render if menu is closed, otherwise render X */}
-        <div className="fixed top-0 right-0 mt-2 mr-2 z-10">
-            <X className={`${menuOpen ? "" : "hidden"} lg:hidden`} onClick={toggleMenu} />
-            <Menu className={`${menuOpen ? "hidden" : ""} lg:hidden`} onClick={toggleMenu} />
-        </div>
+      <div className="fixed top-0 right-0 mt-2 mr-2 z-10">
+        <X className={`${menuOpen ? "" : "hidden"} lg:hidden`} onClick={toggleMenu} />
+        <Menu className={`${menuOpen ? "hidden" : ""} lg:hidden`} onClick={toggleMenu} />
+      </div>
 
-      {/* Nav links for desktop screens */}
       <ul className="hidden lg:flex list-none justify-between ">
         <li className="nav-item ">
-          <Link to="/" className= {currentPage === "/" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600" }>Home</Link>
+          <Link to="/" className={currentPage === "/" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Home</Link>
         </li>
         <li className="nav-item">
-          <Link to="/About" className={currentPage === "/About" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>About</Link>
+          <Link to="/About" className={currentPage === "/About" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>About</Link>
         </li>
         <li className="nav-item">
-          <Link to="/Projects" className={currentPage === "/Projects" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Projects</Link>
+          <Link to="/Projects" className={currentPage === "/Projects" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Projects</Link>
         </li>
         <li className="nav-item">
-          <Link to="/Contact" className={currentPage === "/Contact" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Contact</Link>
+          <Link to="/Contact" className={currentPage === "/Contact" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Contact</Link>
         </li>
         <li className="nav-item">
-          <Link to="/Resume" className={currentPage === "/Resume" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Resume</Link>
+          <Link to="/Resume" className={currentPage === "/Resume" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Resume</Link>
         </li>
       </ul>
 
-      {/* Nav links for mobile screens */}
       <div className={menuOpen ? "fixed top-0 left-0 w-full h-full bg-white transition-all duration-300 ease-in-out transform translate-x-0" : "fixed top-0 left-0 w-64 h-full bg-white transition-all duration-300 ease-in-out transform -translate-x-full"}>
-      <ul className="list-none flex flex-col items-center justify-center space-y-10 pt-20">
-        <li className="nav-item pt-4 pb-0.5">
-          <Link to="/" className={currentPage === "/" ? "nav-link active  text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/About" className={currentPage === "/About" ? "nav-link active  text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>About</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/Projects" className={currentPage === "/Projects" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Projects</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/Contact" className={currentPage === "/Contact" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Contact</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/Resume" className={currentPage === "/Resume" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"}>Resume</Link>
-        </li>
-      </ul>
+        <ul className="list-none flex flex-col items-center justify-center space-y-10 pt-20">
+          <li className="nav-item pt-4 pb-0.5">
+            <Link to="/" className={currentPage === "/" ? "nav-link active  text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/About" className={currentPage === "/About" ? "nav-link active  text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/Projects" className={currentPage === "/Projects" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Projects</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/Contact" className={currentPage === "/Contact" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Contact</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/Resume" className={currentPage === "/Resume" ? "nav-link active text-red-500 hover:text-blue-600" : "nav-link hover:text-blue-600"} onClick={closeMenu}>Resume</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
