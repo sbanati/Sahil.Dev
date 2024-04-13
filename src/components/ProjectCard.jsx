ad85e51cb045dc2af5a647f891cfc01da238eb93
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-13 00:26:29 -0400

    added project props to first 3 cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,23 +1,30 @@
 import PropTypes from 'prop-types';
 import { Github, Share } from 'lucide-react'; // Importing Lucide Icons
 
-const Card = ({ title, description, imageUrl, githubUrl, deployedUrl }) => {
+const Card = ({ title, description, imageUrl, githubUrl, deployedUrl, techStackUrl, index }) => {
+  // Apply custom CSS class to alternate positioning
+  const cardClass = index % 2 === 0 ? 'flex-row-reverse' : 'flex-row';
+
   return (
-    <div className="max-w-5xl mx-auto border border-black rounded-md mb-20 p-8 flex">
-      <div className="w-48 h-auto mr-8 rounded-lg">
-        <img src={imageUrl} alt={title} className="w-full h-full rounded-lg" />
-      </div>
-      <div>
-        <h2 className="text-xl font-bold mb-2">{title}</h2>
-        <p className="text-gray-800 mb-4">{description}</p>
-        <div className="flex justify-between">
-          <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">
+    <div className={` bg-white max-w-5xl shadow-lg mx-auto border rounded-md mb-20 p-8 flex ${cardClass}`}>
+      <div className="flex flex-col justify-center flex-1"> {/* Center the content vertically */}
+        <div className="relative mb-4">
+          <h2 className="text-xl font-bold text-center absolute w-full top-0 -mt-5">{title}</h2> {/* Position title at the top with increased top margin */}
+        </div>
+        <p className="text-black mb-4 mt-4 text-center">{description}</p> {/* Center the description */}
+        <div className="flex justify-center mb-4"> {/* Center the icons */}
+          <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 mr-4">
             <Github size={24} />
           </a>
           <a href={deployedUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">
             <Share size={24} />
           </a>
         </div>
+        {/* Add tech stack image with margin to move it down */}
+        <img src={techStackUrl} alt="Tech Stack" className="w-34 h-auto mx-auto mb-2 mt-10"  /> {/* Use a smaller width for the tech stack image */}
+      </div>
+      <div className="w-72 h-auto ml-8 rounded-lg flex items-center justify-center"> {/* Adjusted width of the image */}
+        <img src={imageUrl} alt={title} className="w-full h-full rounded-lg" /> {/* Adjusted height of the image */}
       </div>
     </div>
   );
@@ -29,6 +36,9 @@ Card.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   githubUrl: PropTypes.string.isRequired,
   deployedUrl: PropTypes.string.isRequired,
+  techStackUrl: PropTypes.string.isRequired, // URL for tech stack image
+  index: PropTypes.number.isRequired, // Added prop for index of the card
 };
 
 export default Card;
+
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,23 +1,35 @@
 import ProjectCard from "../components/ProjectCard";
 import Jate from "../assets/Jate.png"
+import RecipeBlog from "../assets/RecipeBlog.png"
+import WeatherApp from "../assets/WeatherApp.png"
 
 
 const Portfolio = () => {
   // Array data for projects
   const projects = [
     {
-      title: 'Project 1',
-      imageUrl: '',
-      description: 'Description of Project 1',
-      githubUrl: 'https://github.com/yourusername/project1',
-      deployedUrl: 'https://deployment-url.com/project1',
+      title: 'RecipePost',
+      imageUrl: RecipeBlog,
+      description: 'CMS-style platform on Heroku, incorporating (CRUD) operations for blog posts and comments, structured around the (MVC)for creating and managing persistent storage for aseamless user experience',
+      githubUrl: 'https://github.com/topcowmoo/unbreakable-cream',
+      deployedUrl: 'https://unbreakable-cream-a4b95d823c2c.herokuapp.com/',
+      techStackUrl: 'https://skillicons.dev/icons?i=nodejs,mysql,sequelize,figma&theme=light'
     },
     {
       title: 'Jate',
       imageUrl: Jate,
-      description: 'PWA Project',
+      description: 'Create a browser-based text editor as a single-page application (SPA) meeting Progressive Web App (PWA) criteria. Implement data persistence using IndexedDB database with the idb package. Ensure offline functionality and deploy the full-stack application to Render using the Render Deployment Guide.',
       githubUrl: 'https://github.com/sbanati/DraftHub',
       deployedUrl: 'https://drafthub.onrender.com/',
+      techStackUrl: 'https://skillicons.dev/icons?i=nodejs,express,webpack,babel&theme=light',
+    },
+    {
+      title: 'WeatherApp',
+      imageUrl: WeatherApp,
+      description: 'Developing a browser-based weather dashboard featuring dynamically updated HTML and CSS for daily or weekly weather forecasts. Utilizes the openweather API to fetch weather data and localStorage for persistent storage',
+      githubUrl: 'https://github.com/sbanati/Adventure-Forecast',
+      deployedUrl: 'https://sbanati.github.io/Adventure-Forecast/',
+      techStackUrl: 'https://skillicons.dev/icons?i=js,html,css&theme=light',
     },
     {
       title: 'Project 2',
@@ -25,6 +37,7 @@ const Portfolio = () => {
       description: 'Description of Project 2',
       githubUrl: 'https://github.com/yourusername/project2',
       deployedUrl: 'https://deployment-url.com/project2',
+      techStackUrl: '',
     },
     {
       title: 'Project 2',
@@ -32,6 +45,7 @@ const Portfolio = () => {
       description: 'Description of Project 2',
       githubUrl: 'https://github.com/yourusername/project2',
       deployedUrl: 'https://deployment-url.com/project2',
+      techStackUrl: '',
     },
     {
       title: 'Project 2',
@@ -39,20 +53,14 @@ const Portfolio = () => {
       description: 'Description of Project 2',
       githubUrl: 'https://github.com/yourusername/project2',
       deployedUrl: 'https://deployment-url.com/project2',
-    },
-    {
-      title: 'Project 2',
-      imageUrl: '',
-      description: 'Description of Project 2',
-      githubUrl: 'https://github.com/yourusername/project2',
-      deployedUrl: 'https://deployment-url.com/project2',
+      techStackUrl: '',
     },
    
     
   ];
 
   return (
-    <div className="bg-neutral-100 pt-8 pb-8">
+    <div className="bg-neutral-100 pt-8 pb-12">
       <div className="max-w-5xl mx-auto">
         {projects.map((project, index) => (
           <ProjectCard
@@ -63,6 +71,7 @@ const Portfolio = () => {
             githubUrl={project.githubUrl}
             deployedUrl={project.deployedUrl}
             index={index} 
+            techStackUrl={project.techStackUrl}
           />
         ))}
       </div>
@@ -71,3 +80,5 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+
